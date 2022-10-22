4a3f7d2091a0ddf66c61ac1b7f84a546b0ed4204
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-10-22 18:49:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './SharedLayout';
 import Home from '../pages/Home';
 import Movies from '../pages/Movies';
 import MovieDetails from './MovieDetails';
 import Cast from './Cast';
 import Reviews from './Reviews';
-import { LinkItem } from './App.styled';
 
 export const App = () => {
   return (
     <div>
-      <nav>
-        <LinkItem to="/" end>
-          Home
-        </LinkItem>
-
-        <LinkItem to="/movies">Moveis</LinkItem>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />{' '}
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />{' '}
+          </Route>
+          <Route path="*" element={<Home />} />
         </Route>
-        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
