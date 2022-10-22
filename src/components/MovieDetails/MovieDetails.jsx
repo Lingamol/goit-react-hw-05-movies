22253e7443107adb5cde713b4a869b23ae1923f7
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-10-23 01:35:34 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,30 @@
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { NavWrapper } from './MovieDetails.styled';
+import { fetchMoviesById } from 'services/api';
+import { useState, useEffect } from 'react';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   console.log('movieId', movieId);
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/movies';
+  const [selectedMovie, setSelectedMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchSelectedMovie() {
+      try {
+        const response = await fetchSelectedMovie(movieId);
+        if (response) {
+          setSelectedMovie(response.results);
+          console.log('setMovieList', selectedMovie);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchSelectedMovie();
+  }, [movieId]);
   // console.log('location ', location);
   // console.log('location.state', location.state);
   // console.log('backLinkHref', backLinkHref);
